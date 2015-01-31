61895b5e06f8f5cbd259cfee60a46a7c3d25cea0
Boris Smus <boris@smus.com>
2015-01-30 17:22:01 -0800

    Updated deps.

--- update.sh
+++ update.sh
@@ -5,4 +5,4 @@
 curl -o js/VRControls.js https://raw.githubusercontent.com/mrdoob/three.js/master/examples/js/controls/VRControls.js
 curl -o js/VREffect.js https://raw.githubusercontent.com/mrdoob/three.js/master/examples/js/effects/VREffect.js
 curl -o js/three.min.js http://threejs.org/build/three.min.js
-curl -o js/webvr-polyfill.js https://github.com/borismus/webvr-polyfill/blob/master/build/webvr-polyfill.js
+curl -o js/webvr-polyfill.js https://raw.githubusercontent.com/borismus/webvr-polyfill/master/build/webvr-polyfill.js
