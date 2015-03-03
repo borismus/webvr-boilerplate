3d6d0cb034c9b195f141ac6b7c4b69d23b9f5a52
Boris Smus <boris@smus.com>
2015-03-03 09:17:27 -0800

    Changed to SVG, fetched updated VREffect

--- update.sh
+++ update.sh
@@ -3,6 +3,6 @@
 # three.js repository.
 
 curl -o js/VRControls.js https://raw.githubusercontent.com/mrdoob/three.js/master/examples/js/controls/VRControls.js
-curl -o js/VREffect.js https://raw.githubusercontent.com/mrdoob/three.js/master/examples/js/effects/VREffect.js
+curl -o js/VREffect.js https://raw.githubusercontent.com/mrdoob/three.js/dev/examples/js/effects/VREffect.js
 curl -o js/three.min.js http://threejs.org/build/three.min.js
 curl -o js/webvr-polyfill.js https://raw.githubusercontent.com/borismus/webvr-polyfill/master/build/webvr-polyfill.js
