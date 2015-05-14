262d5de95cc6a83fe5e33c52f96e7f4ce8dde156
Boris Smus <boris@smus.com>
2015-05-14 16:55:52 -0700

    Testing proper lens position for iOS.

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
 default:
-	mkdir -p build
-	browserify src/main.js > build/webvr-manager.js
+		mkdir -p build
+		browserify src/main.js > build/webvr-manager.js
+
+device:
+		browserify test/device-info-test.js > build/device-info-test.js
 
 clean:
-	rm build/webvr-manager.js
-	rmdir build
+		rm build/webvr-manager.js
+		rm build/device-info-test.js
+		rmdir build
---
+++ test/device-info-test.js
@@ -0,0 +1,22 @@
+var DeviceInfo = require('../src/device-info.js');
+
+var di = new DeviceInfo();
+var centroid = di.getLeftCentroid();
+
+// Size the canvas. Render the centroid.
+var canvas = document.querySelector('canvas');
+var w = window.innerWidth;
+var h = window.innerHeight;
+var x = centroid.x * w/2;
+var y = centroid.y * h;
+var size = 10;
+
+canvas.width = w;
+canvas.height = h;
+
+var ctx = canvas.getContext('2d');
+ctx.clearRect(0, 0, w, h);
+ctx.fillStyle = 'black';
+ctx.fillRect(x - size/2, y - size/2, size, size);
+
+console.log('Placing eye at (%d, %d).', x, y);
