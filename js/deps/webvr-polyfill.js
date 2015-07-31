f56f750f53f27556a3fed767178c375c984b6c37
Boris Smus <boris@smus.com>
2015-07-31 16:16:47 -0700

    Eliminated debugging output.

--- js/deps/webvr-polyfill.js
+++ js/deps/webvr-polyfill.js
@@ -500,8 +500,8 @@ PosePredictor.prototype.getPrediction = function(currentQ, timestamp) {
 
       // For debugging, report the abs. difference between actual and predicted
       // angles.
-      var angleDelta = THREE.Math.radToDeg(Math.abs(predictAngle - angle));
-      console.log('|Actual-Predicted| = %d deg', angleDelta);
+      //var angleDelta = THREE.Math.radToDeg(Math.abs(predictAngle - angle));
+      //console.log('|Actual-Predicted| = %d deg', angleDelta);
 
       // Save the current quaternion for later.
       this.lastQ.copy(currentQ);
