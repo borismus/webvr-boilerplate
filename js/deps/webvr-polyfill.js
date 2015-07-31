bde3a32d8f33d2c7fd50bf0880bfa9f72cb2cddf
Boris Smus <boris@smus.com>
2015-07-31 10:28:36 -0700

    Reduce prediction time

--- js/deps/webvr-polyfill.js
+++ js/deps/webvr-polyfill.js
@@ -398,6 +398,21 @@ MouseKeyboardPositionSensorVRDevice.prototype.isPointerLocked_ = function() {
 module.exports = MouseKeyboardPositionSensorVRDevice;
 
 },{"./base.js":1,"./three-math.js":7}],6:[function(require,module,exports){
+/*
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // How much to interpolate between the current orientation estimate and the
 // previous estimate position. This is helpful for devices with low
 // deviceorientation firing frequency (eg. on iOS, it is 20 Hz).  The larger
@@ -410,7 +425,7 @@ var PREDICTION_SMOOTHING_FACTOR = 0.01;
 var PREDICTION_THRESHOLD_DEG = 0.0001;
 
 // How far into the future to predict.
-var PREDICTION_TIME_MS = 50;
+var PREDICTION_TIME_MS = 30;
 
 var Modes = {
   NONE: 0,
