3a90d5bfa53e9f0998069202f70f469e403f9a10
Boris Smus <boris@smus.com>
2015-08-06 10:22:56 -0700

    update polyfill

--- js/deps/webvr-polyfill.js
+++ js/deps/webvr-polyfill.js
@@ -588,16 +588,18 @@ PosePredictor.prototype.getAxisAngularSpeedFromRotationRate_ = function(rotation
     return null;
   }
   // Get axis and angular speed from rotation rate.
-  // TODO: Take into account the screen orientation too!
   var vec = new THREE.Vector3(rotationRate.beta, rotationRate.alpha, -rotationRate.gamma);
+
+  if (/iPad|iPhone|iPod/.test(navigator.platform)) {
+    // TODO: iOS is somehow different. But how?
+    vec.y = -vec.y;
+  }
+  // Take into account the screen orientation too!
   vec.applyQuaternion(this.screenTransform);
 
   // Angular speed in deg/s.
   var angularSpeedDegS = vec.length();
-  if (/iPad|iPhone|iPod/.test(navigator.platform)) {
-    // TODO: iOS is somehow different. But how?
-    //vec.y = -vec.y;
-  }
+
   var axis = vec.normalize();
   return {
     speed: angularSpeedDegS,
