63a2d23b8d63dced9bb67aaad3670280712e34c9
Boris Smus <boris@smus.com>
2015-09-15 16:26:10 -0700

    Version bump!

--- update.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Fetch the latest versions of three.js, VRControls and VREffect from the
-# three.js repository.
-
-curl -o js/deps/VRControls.js https://raw.githubusercontent.com/mrdoob/three.js/master/examples/js/controls/VRControls.js
-curl -o js/deps/VREffect.js https://raw.githubusercontent.com/mrdoob/three.js/master/examples/js/effects/VREffect.js
-curl -o js/deps/three.js https://raw.githubusercontent.com/mrdoob/three.js/master/build/three.js
