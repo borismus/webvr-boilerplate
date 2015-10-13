2bbd49f4c9e1c50878399d30177f214cc023582a
Boris Smus <boris@smus.com>
2015-10-12 18:37:57 -0700

    Renamed to button manager. Update assets.

---
+++ src/button-manager.js
@@ -0,0 +1,155 @@
+/*
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var Modes = require('./modes.js');
+var Emitter = require('./emitter.js');
+var Util = require('./util.js');
+
+/**
+ * Everything having to do with the WebVR button.
+ * Emits a 'click' event when it's clicked.
+ */
+function ButtonManager() {
+  this.loadIcons_();
+
+  // Make the fullscreen button.
+  var fsButton = this.createButton();
+  fsButton.src = this.ICONS.fullscreen;
+  fsButton.title = 'Fullscreen mode';
+  var s = fsButton.style;
+  s.bottom = 0;
+  s.right = 0;
+  fsButton.addEventListener('click', this.createClickHandler_('fs'));
+  document.body.appendChild(fsButton);
+  this.fsButton = fsButton;
+
+  // Make the VR button.
+  var vrButton = this.createButton();
+  vrButton.src = this.ICONS.cardboard;
+  vrButton.title = 'Virtual reality mode';
+  var s = vrButton.style;
+  s.bottom = 0;
+  s.right = '48px';
+  vrButton.addEventListener('click', this.createClickHandler_('vr'));
+  document.body.appendChild(vrButton);
+  this.vrButton = vrButton;
+
+  // Make the back button.
+  var backButton = this.createButton();
+  backButton.title = 'Back to previous mode';
+  var s = backButton.style;
+  s.left = 0;
+  s.top = 0;
+  backButton.src = this.ICONS.back;
+  backButton.addEventListener('click', this.createClickHandler_('back'));
+  document.body.appendChild(backButton);
+  this.backButton = backButton;
+
+  this.isVisible = true;
+
+}
+ButtonManager.prototype = new Emitter();
+
+ButtonManager.prototype.createButton = function() {
+  var button = document.createElement('img');
+  var s = button.style;
+  s.position = 'fixed';
+  s.width = '24px'
+  s.height = '24px';
+  s.backgroundSize = 'cover';
+  s.backgroundColor = 'transparent';
+  s.border = 0;
+  s.userSelect = 'none';
+  s.webkitUserSelect = 'none';
+  s.MozUserSelect = 'none';
+  s.cursor = 'pointer';
+  s.padding = '12px';
+  s.zIndex = 1;
+  s.display = 'none';
+
+  // Prevent button from being selected and dragged.
+  button.draggable = false;
+  button.addEventListener('dragstart', function(e) {
+    e.preventDefault();
+  });
+
+  // Style it on hover.
+  button.addEventListener('mouseenter', function(e) {
+    /* -webkit-filter: drop-shadow(0 0 5px rgba(255,255,255,1)); */
+    s.webkitFilter = 'drop-shadow(0 0 5px rgba(255,255,255,1))';
+  });
+  button.addEventListener('mouseleave', function(e) {
+    s.webkitFilter = '';
+  });
+  return button;
+};
+
+ButtonManager.prototype.setMode = function(mode, isVRCompatible) {
+  if (!this.isVisible) {
+    return;
+  }
+  switch (mode) {
+    case Modes.NORMAL:
+      this.fsButton.style.display = 'block';
+      this.fsButton.src = this.ICONS.fullscreen;
+      //this.vrButton.style.display = (isVRCompatible ? 'block' : 'none');
+      // For now, just disable direct-to-VR mode.
+      this.vrButton.style.display = 'none';
+      this.backButton.style.display = 'none';
+      break;
+    case Modes.MAGIC_WINDOW:
+      this.fsButton.style.display = 'block';
+      this.fsButton.src = this.ICONS.exitFullscreen;
+      this.vrButton.style.display = (isVRCompatible ? 'block' : 'none');
+      this.backButton.style.display = 'block';
+      break;
+    case Modes.VR:
+      this.fsButton.style.display = 'none';
+      this.vrButton.style.display = 'none';
+      this.backButton.style.display = 'block';
+      break;
+  }
+
+  // Hack for Safari Mac/iOS to force relayout (svg-specific issue)
+  // http://goo.gl/hjgR6r
+  var oldValue = this.fsButton.style.display;
+  this.fsButton.style.display = 'inline-block';
+  this.fsButton.offsetHeight;
+  this.fsButton.style.display = oldValue;
+};
+
+ButtonManager.prototype.setVisibility = function(isVisible) {
+  this.isVisible = isVisible;
+  this.fsButton.style.display = isVisible ? 'block' : 'none';
+};
+
+ButtonManager.prototype.createClickHandler_ = function(eventName) {
+  return function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    this.emit(eventName);
+  }.bind(this);
+};
+
+ButtonManager.prototype.loadIcons_ = function() {
+  // Preload some hard-coded SVG.
+  this.ICONS = {};
+  this.ICONS.cardboard = Util.base64('image/svg+xml', 'PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNHB4IiBoZWlnaHQ9IjI0cHgiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0iI0ZGRkZGRiI+CiAgICA8cGF0aCBkPSJNMjAuNzQgNkgzLjIxQzIuNTUgNiAyIDYuNTcgMiA3LjI4djEwLjQ0YzAgLjcuNTUgMS4yOCAxLjIzIDEuMjhoNC43OWMuNTIgMCAuOTYtLjMzIDEuMTQtLjc5bDEuNC0zLjQ4Yy4yMy0uNTkuNzktMS4wMSAxLjQ0LTEuMDFzMS4yMS40MiAxLjQ1IDEuMDFsMS4zOSAzLjQ4Yy4xOS40Ni42My43OSAxLjExLjc5aDQuNzljLjcxIDAgMS4yNi0uNTcgMS4yNi0xLjI4VjcuMjhjMC0uNy0uNTUtMS4yOC0xLjI2LTEuMjh6TTcuNSAxNC42MmMtMS4xNyAwLTIuMTMtLjk1LTIuMTMtMi4xMiAwLTEuMTcuOTYtMi4xMyAyLjEzLTIuMTMgMS4xOCAwIDIuMTIuOTYgMi4xMiAyLjEzcy0uOTUgMi4xMi0yLjEyIDIuMTJ6bTkgMGMtMS4xNyAwLTIuMTMtLjk1LTIuMTMtMi4xMiAwLTEuMTcuOTYtMi4xMyAyLjEzLTIuMTNzMi4xMi45NiAyLjEyIDIuMTMtLjk1IDIuMTItMi4xMiAyLjEyeiIvPgogICAgPHBhdGggZmlsbD0ibm9uZSIgZD0iTTAgMGgyNHYyNEgwVjB6Ii8+Cjwvc3ZnPgo=');
+  this.ICONS.fullscreen = Util.base64('image/svg+xml', 'PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNHB4IiBoZWlnaHQ9IjI0cHgiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0iI0ZGRkZGRiI+CiAgICA8cGF0aCBkPSJNMCAwaDI0djI0SDB6IiBmaWxsPSJub25lIi8+CiAgICA8cGF0aCBkPSJNNyAxNEg1djVoNXYtMkg3di0zem0tMi00aDJWN2gzVjVINXY1em0xMiA3aC0zdjJoNXYtNWgtMnYzek0xNCA1djJoM3YzaDJWNWgtNXoiLz4KPC9zdmc+Cg==');
+  this.ICONS.exitFullscreen = Util.base64('image/svg+xml', 'PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNHB4IiBoZWlnaHQ9IjI0cHgiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0iI0ZGRkZGRiI+CiAgICA8cGF0aCBkPSJNMCAwaDI0djI0SDB6IiBmaWxsPSJub25lIi8+CiAgICA8cGF0aCBkPSJNNSAxNmgzdjNoMnYtNUg1djJ6bTMtOEg1djJoNVY1SDh2M3ptNiAxMWgydi0zaDN2LTJoLTV2NXptMi0xMVY1aC0ydjVoNVY4aC0zeiIvPgo8L3N2Zz4K');
+  this.ICONS.back = Util.base64('image/svg+xml', 'PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNHB4IiBoZWlnaHQ9IjI0cHgiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0iI0ZGRkZGRiI+CiAgICA8cGF0aCBkPSJNMCAwaDI0djI0SDB6IiBmaWxsPSJub25lIi8+CiAgICA8cGF0aCBkPSJNMjAgMTFINy44M2w1LjU5LTUuNTlMMTIgNGwtOCA4IDggOCAxLjQxLTEuNDFMNy44MyAxM0gyMHYtMnoiLz4KPC9zdmc+Cg==');
+};
+
+module.exports = ButtonManager;
