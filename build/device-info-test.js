404ba792aeb4f744462ce15bf6cf8e213d93c211
Boris Smus <boris@smus.com>
2015-12-15 17:23:50 -0800

    Add device tests.

--- build/device-info-test.js
+++ build/device-info-test.js
@@ -1,42 +1,111 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/*
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 var Util = require('./util.js');
 
-// Width, height and bevel measurements done on real iPhones.
+// Display width, display height and bevel measurements done on real phones.
 // Resolutions from http://www.paintcodeapp.com/news/ultimate-guide-to-iphone-resolutions
-// Note: iPhone pixels are not square, so relying on diagonal is not enough.
-var Devices = {
+var iOSDevices = {
   iPhone5: new Device({
     width: 640,
     height: 1136,
-    widthMm: 51.27,
-    heightMm: 90.11,
-    bevelMm: 3.96
+    widthMeters: 0.05127,
+    heightMeters: 0.09011,
+    bevelMeters: 0.00396
   }),
   iPhone6: new Device({
     width: 750,
     height: 1334,
-    widthMm: 58.4,
-    heightMm: 103.8,
-    bevelMm: 3.71
+    widthMeters: 0.0584,
+    heightMeters: 0.1038,
+    bevelMeters: 0.00371
   }),
   iPhone6Plus: new Device({
     width: 1242,
     height: 2208,
-    widthMm: 69.54,
-    heightMm: 122.35,
-    bevelMm: 4.62
+    widthMeters: 0.06954,
+    heightMeters: 0.12235,
+    bevelMeters: 0.00462
   })
 };
 
-var Enclosures = {
-  CardboardV1: new CardboardEnclosure({
-    ipdMm: 61,
-    baselineLensCenterMm: 37.26
+// TODO: Add Nexus 5X, Nexus 6P, Nexus 6.
+var AndroidDevices = {
+  Nexus5: new Device({
+    userAgentRegExp: /Nexus 5/,
+    widthMeters: 0.062,
+    heightMeters: 0.110,
+    bevelMeters: 0.004
   }),
-  FunkyMonkey: new CardboardEnclosure({
+  GalaxyS3: new Device({
+    userAgentRegExp: /GT-I9300/,
+    widthMeters: 0.060,
+    heightMeters: 0.106,
+    bevelMeters: 0.005
+  }),
+  GalaxyS4: new Device({
+    userAgentRegExp: /GT-I9505/,
+    widthMeters: 0.0625,
+    heightMeters: 0.111,
+    bevelMeters: 0.004
+  }),
+  GalaxyS5: new Device({
+    userAgentRegExp: /SM-G900F/,
+    widthMeters: 0.066,
+    heightMeters: 0.113,
+    bevelMeters: 0.005
+  }),
+  GalaxyS6: new Device({
+    userAgentRegExp: /SM-G920/,
+    widthMeters: 0.0635,
+    heightMeters: 0.114,
+    bevelMeters: 0.0035
+  }),
+};
+
+var Viewers = {
+  CardboardV1: new CardboardViewer({
+    id: 'CardboardV1',
+    label: 'Cardboard I/O 2014',
+    fov: 40,
+    interLensDistance: 0.060,
+    baselineLensDistance: 0.035,
+    screenLensDistance: 0.042,
+    distortionCoefficients: [0.441, 0.156],
+    inverseCoefficients: [-0.4410035, 0.42756155, -0.4804439, 0.5460139,
+      -0.58821183, 0.5733938, -0.48303202, 0.33299083, -0.17573841,
+      0.0651772, -0.01488963, 0.001559834]
+  }),
+  CardboardV2: new CardboardViewer({
+    id: 'CardboardV2',
+    label: 'Cardboard I/O 2015',
+    fov: 60,
+    interLensDistance: 0.064,
+    baselineLensDistance: 0.035,
+    screenLensDistance: 0.039,
+    distortionCoefficients: [0.34, 0.55],
+    inverseCoefficients: [-0.33836704, -0.18162185, 0.862655, -1.2462051,
+      1.0560602, -0.58208317, 0.21609078, -0.05444823, 0.009177956,
+      -9.904169E-4, 6.183535E-5, -1.6981803E-6]
   })
 };
 
+
+var DEFAULT_LEFT_CENTER = {x: 0.5, y: 0.5};
+var DEFAULT_RIGHT_CENTER = {x: 0.5, y: 0.5};
 
 /**
  * Gives the correct device DPI based on screen dimensions and user agent.
@@ -44,36 +113,61 @@ var Enclosures = {
  */
 function DeviceInfo() {
   this.device = this.determineDevice_();
-  this.enclosure = Enclosures.CardboardV1;
+  this.viewer = Viewers.CardboardV1;
 }
 
+DeviceInfo.prototype.getDevice = function() {
+  return this.device;
+};
+
+DeviceInfo.prototype.setViewer = function(viewer) {
+  this.viewer = viewer;
+};
+
 /**
- * Gets the coordinates (in [0, 1]) for the left eye.
+ * Gets the coordinates (in [0, 1]) for the left eye. To go from these
+ * normalized coordinates, you multiply by real dimensions.
  */
-DeviceInfo.prototype.getLeftCentroid = function() {
-  // Get parameters from the enclosure.
-  var eyeToMid = this.enclosure.ipdMm / 2;
-  var eyeToBase = this.enclosure.baselineLensCenterMm;
+DeviceInfo.prototype.getLeftEyeCenter = function() {
+  if (!this.device) {
+    return DEFAULT_LEFT_CENTER;
+  }
+  // Get parameters from the viewer.
+  var ipdMeters = this.viewer.interLensDistance;
+  var eyeToMid = ipdMeters / 2;
+  var eyeToBase = this.viewer.baselineLensDistance;
 
   // Get parameters from the phone.
-  var halfWidthMm = this.device.heightMm / 2;
-  var heightMm = this.device.widthMm;
+  var halfWidthMeters = this.device.heightMeters / 2;
+  var heightMeters = this.device.widthMeters;
 
   // Do calculations.
   // Measure the distance between bottom of screen and center.
-  var eyeToBevel = eyeToBase - this.device.bevelMm;
-  var px = 1 - (eyeToMid / halfWidthMm);
-  var py = 1 - (eyeToBevel / heightMm);
+  var eyeToBevel = eyeToBase - this.device.bevelMeters;
+  var px = 1 - (eyeToMid / halfWidthMeters);
+  var py = 1 - (eyeToBevel / heightMeters);
 
   return {x: px, y: py};
 };
 
+DeviceInfo.prototype.getRightEyeCenter = function() {
+  if (!this.device) {
+    return DEFAULT_RIGHT_CENTER;
+  }
+  var left = this.getLeftEyeCenter();
+  return {x: 1 - left.x, y: left.y};
+};
+
 DeviceInfo.prototype.determineDevice_ = function() {
   // Only support iPhones.
-  if (!Util.isIOS()) {
-    return null;
+  if (Util.isIOS()) {
+    return this.determineIPhone_();
+  } else {
+    return this.determineAndroid_();
   }
+};
 
+DeviceInfo.prototype.determineIPhone_ = function() {
   // On iOS, use screen dimensions to determine iPhone/iPad model.
   var userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
@@ -84,8 +178,8 @@ DeviceInfo.prototype.determineDevice_ = function() {
   var pixelHeight = height * window.devicePixelRatio;
 
   // Match the screen dimension to the correct device.
-  for (var id in Devices) {
-    var device = Devices[id];
+  for (var id in iOSDevices) {
+    var device = iOSDevices[id];
     // Expect an exact match on width.
     if (device.width == pixelWidth || device.width == pixelHeight) {
       console.log('Detected iPhone: %s', id);
@@ -93,33 +187,80 @@ DeviceInfo.prototype.determineDevice_ = function() {
       return device;
     }
   }
+  // This should never happen.
+  console.error('Unable to detect iPhone type.');
+  return null;
+};
+
+DeviceInfo.prototype.determineAndroid_ = function() {
+  // Do a userAgent match against all of the known Android devices.
+  for (var id in AndroidDevices) {
+    var device = AndroidDevices[id];
+    // Does it match?
+    if (navigator.userAgent.match(device.userAgentRegExp)) {
+      console.log('Detected Android: %s', id);
+      return device;
+    }
+  }
+  // No device matched.
   return null;
 };
 
 
-
 function Device(params) {
+  this.userAgentRegExp = params.userAgentRegExp;
+
   this.width = params.width;
   this.height = params.height;
-  this.widthMm = params.widthMm;
-  this.heightMm = params.heightMm;
-  this.bevelMm = params.bevelMm;
+  this.widthMeters = params.widthMeters;
+  this.heightMeters = params.heightMeters;
+  this.bevelMeters = params.bevelMeters;
 }
 
 
-function CardboardEnclosure(params) {
+function CardboardViewer(params) {
+  // A machine readable ID.
+  this.id = params.id;
+  // A human readable label.
+  this.label = params.label;
+
+  // Field of view in degrees (per side).
+  this.fov = params.fov;
+
+  // Distance between lens centers in meters.
+  this.interLensDistance = params.interLensDistance;
+  // Distance between viewer baseline and lens center in meters.
+  this.baselineLensDistance = params.baselineLensDistance;
+  // Screen-to-lens distance in meters.
+  this.screenLensDistance = params.screenLensDistance;
+
   // Distortion coefficients.
-  this.k1 = params.k1;
-  this.k2 = params.k2;
-  // IPD in millimeters.
-  this.ipdMm = params.ipdMm;
-  // Distance between baseline and lens.
-  this.baselineLensCenterMm = params.baselineLensCenterMm;
+  this.distortionCoefficients = params.distortionCoefficients;
+  // Inverse distortion coefficients.
+  // TODO: Calculate these from distortionCoefficients in the future.
+  this.inverseCoefficients = params.inverseCoefficients;
 }
 
+// Export viewer information.
+DeviceInfo.Viewers = Viewers;
 module.exports = DeviceInfo;
 
 },{"./util.js":2}],2:[function(require,module,exports){
+/*
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 var Util = {};
 
 Util.base64 = function(mimeType, base64) {
@@ -132,9 +273,41 @@ Util.isMobile = function() {
   return check;
 };
 
+Util.isFirefox = function() {
+  return /firefox/i.test(navigator.userAgent);
+};
+
 Util.isIOS = function() {
   return /(iPad|iPhone|iPod)/g.test(navigator.userAgent);
 };
+
+Util.isIFrame = function() {
+  try {
+    return window.self !== window.top;
+  } catch (e) {
+    return true;
+  }
+};
+
+Util.appendQueryParameter = function(url, key, value) {
+  // Determine delimiter based on if the URL already GET parameters in it.
+  var delimiter = (url.indexOf('?') < 0 ? '?' : '&');
+  url += delimiter + key + '=' + value;
+  return url;
+};
+
+// From http://goo.gl/4WX3tg
+Util.getQueryParameter = function(name) {
+  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+      results = regex.exec(location.search);
+  return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+};
+
+Util.isLandscapeMode = function() {
+  return (window.orientation == 90 || window.orientation == -90);
+};
+
 
 module.exports = Util;
 
@@ -142,7 +315,7 @@ module.exports = Util;
 var DeviceInfo = require('../src/device-info.js');
 
 var di = new DeviceInfo();
-var centroid = di.getLeftCentroid();
+var centroid = di.getLeftEyeCenter();
 
 // Size the canvas. Render the centroid.
 var canvas = document.querySelector('canvas');
